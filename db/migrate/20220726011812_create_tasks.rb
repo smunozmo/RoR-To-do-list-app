d2c687be6b0006f352530deaad3203d87b5f3435
Sebastián Muñoz <sebastian@Sebastians-MacBook-Air.local>
2022-07-25 20:21:53 -0500

    Create Tag model.

---
+++ db/migrate/20220726011812_create_tasks.rb
@@ -0,0 +1,13 @@
+class CreateTasks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tasks do |t|
+      t.text :title
+      t.string :status
+      t.integer :level
+      t.datetime :deadline
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
