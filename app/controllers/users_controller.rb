95efb36585482433f7ca4d2c68aea592f014e840
Sebastián Muñoz <sebastian@Sebastians-MacBook-Air.local>
2022-07-26 00:30:52 -0500

    Add filter by tag feature.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,14 @@ class UsersController < ApplicationController
     @taggables = Taggable.all
   end
 
+  def filter_tag
+    @user = User.find(params[:user_id])
+    @tasks = Task.joins(:user).where(users: { id: params[:user_id] })
+    @tags = Tag.joins(:user).where(users: { id: params[:user_id] })
+    @taggables = Taggable.all
+    @filter_tag = Tag.find(params[:tag_id])
+  end
+
   def tags
     @user = User.find(params[:user_id])
     @tags = Tag.joins(:user).where(users: { id: params[:user_id] })
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'users/:user_id/tasks', to: "users#tasks", as: "user_tasks"
   get 'users/:user_id/tags', to: "users#tags", as: "user_tags"
+  get "users/:user_id/tags/:tag_id/tasks", to: "users#filter_tag", as: "filter_tag"
   
   get 'users/:user_id/tasks/new', to: "tasks#new", as: "new_task"
   post 'users/:user_id/tasks/new', to: "tasks#create", as: "create_task"
